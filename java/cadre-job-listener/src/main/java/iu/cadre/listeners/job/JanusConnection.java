9b832e88b7b2615bd9580d6559ec3892e06eb1de
cpelikan <cpelikan@iu.edu>
2020-03-13 14:00:57 -0400

    update with GuangChen's code

--- java/cadre-job-listener/src/main/java/iu/cadre/listeners/job/JanusConnection.java
+++ java/cadre-job-listener/src/main/java/iu/cadre/listeners/job/JanusConnection.java
@@ -1,25 +1,71 @@
 package iu.cadre.listeners.job;
 
-import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
-
-import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.janusgraph.core.JanusGraph;
+import org.janusgraph.core.JanusGraphFactory;
+import org.janusgraph.core.JanusGraphTransaction;
+import org.janusgraph.core.schema.JanusGraphManagement;
+import org.janusgraph.graphdb.database.StandardJanusGraph;
 
-import java.util.Map;
+import java.util.Iterator;
 
 public class JanusConnection {
     public static void main(String[] args) {
         try {
-            GraphTraversalSource g = traversal().withRemote(DriverRemoteConnection.using("conf/remote-objects.yaml  ", "mag_traversal"));
-            GraphTraversal<Vertex, Map<Object, Object>> valueMap = g.V().has("Paper", "paperTitle", "full case study report upplandsbondens sweden")
-                    .valueMap("year", "paperTitle", "referenceCount", "rank", "citationCount", "createdDate", "paperId",
-                            "originalTitle", "date", "estimatedCitation", "languageCodes", "urls");
-            valueMap.forEachRemaining(
-                    System.out::println
-            );
-            g.close();
+//            JanusGraphFactory.
+//            GraphTraversalSource g = traversal().withRemote(DriverRemoteConnection.using("conf/remote-objects.yaml", "mag_traversal"));
+//            GraphTraversal<Vertex, Map<Object, Object>> valueMap = g.V().has("Paper", "paperTitle", "full case study report upplandsbondens sweden")
+//                    .valueMap("year", "paperTitle", "referenceCount", "rank", "citationCount", "createdDate", "paperId",
+//                            "originalTitle", "date", "estimatedCitation", "languageCodes", "urls");
+//            valueMap.forEachRemaining(
+//                    System.out::println
+//            );
+//            g.close();
+
+            if (null == args || args.length != 1) {
+                System.err.println(
+                        "Usage: JanusGraphConnSample <janusgraph-config-file>");
+                System.exit(1);
+            }
+
+            final JanusGraph graph = JanusGraphFactory.open(args[0]);
+
+            StandardJanusGraph standardGraph = (StandardJanusGraph) graph;
+
+            // get graph management
+            JanusGraphManagement mgmt = standardGraph.openManagement();
+
+            // you code using 'mgmt' to perform any management related operations
+            // bla bla bla
+
+            // using graph to do traversal
+            JanusGraphTransaction graphTransaction = graph.newTransaction();
+
+            GraphTraversalSource traversal = graphTransaction.traversal();
+
+            String vertexLabel = "Paper";
+            String fieldName = "paperTitle";
+            String fieldValue = "big data technologies a survey";
+
+            Iterator<Vertex> nodes = traversal.V().has(vertexLabel, fieldName,
+                    fieldValue);
+
+            int count = 0;
+            while (nodes.hasNext()) {
+                Vertex v = nodes.next();
+                count++;
+                // use apis on Vertex to explore the node, below is the API link
+                // http://tinkerpop.apache.org/javadocs/3.2.3/full/org/apache/tinkerpop/gremlin/structure/Vertex.html#properties-java.lang.String...-
+            }
+            System.out.println(count);
+
+            // if you modify anything, always commit, for read only query, it is not needed
+            // graphTransaction.commit();
+
+            // close transaction when finishing using it
+            graphTransaction.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
