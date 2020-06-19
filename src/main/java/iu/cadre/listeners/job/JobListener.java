18e42df110959bd2caa16940caf6539895f2ea07
Ubuntu <ubuntu@ip-10-0-1-105.us-east-2.compute.internal>
2020-06-19 17:22:40 +0000

    Make the maxContentLength larger
    Fix errors with Janus sending too much data at once. Also:
    
    Don't connect to metadatabase until a message is received.
    Use a Cluster to initialize the remote connection rather than a file.
    Delete the sqs message even if an error is thrown.
    Better logging.

--- src/main/java/iu/cadre/listeners/job/JobListener.java
+++ src/main/java/iu/cadre/listeners/job/JobListener.java
@@ -3,8 +3,17 @@ package iu.cadre.listeners.job;
 import com.google.gson.JsonParser;
 import iu.cadre.listeners.job.util.ConfigReader;
 import iu.cadre.listeners.job.util.ListenerUtils;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV1d0;
+import org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV3d0;
+import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoMapper;
 import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerIoRegistryV3d0;
+import org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry;
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
@@ -34,8 +43,7 @@ public class JobListener {
             ConfigReader.loadProperties(args[0]);
             poll_queue();
         } catch (Exception e) {
-            System.err.println("FATAL ERROR");
-            e.printStackTrace();
+            LOG.error("FATAL ERROR, exiting", e);
             System.exit(-1);
         }
     }
@@ -63,9 +71,9 @@ public class JobListener {
                 .build();
 
         LOG.info("SQS connection established and listening");
-        JobStatus status = new JobStatus();
         while (true) {
             List<Message> messages = sqsClient.receiveMessage(receiveRequest).messages();
+            JobStatus status = new JobStatus();
             // print out the messages
             for (Message m : messages) {
                 String messageBody = m.body();
@@ -87,18 +95,30 @@ public class JobListener {
                     String fileName = getFileName(query.JobId(), query.JobName());
                     String csvPath = userQueryResultDir + File.separator + fileName + ".csv";
                     String graphMLFile = userQueryResultDir + File.separator + fileName + ".xml";
-                    LOG.info(graphMLFile);
 
                     if (query.DataSet().equals("mag")) {
-                        janusTraversal = (GraphTraversalSource) EmptyGraph.instance().traversal().withRemote("conf/remote-graph.properties");
+                        LOG.info("Connecting to Janus server");
+                        Cluster cluster = Cluster.build()
+                                .addContactPoint("localhost")
+                                .port(8182)
+                                .maxContentLength(10000000)
+                                .serializer(new GryoMessageSerializerV3d0(GryoMapper.build()
+                                        .addRegistry(JanusGraphIoRegistry.instance())
+                                        .addRegistry(TinkerIoRegistryV3d0.instance())))
+                                .create();
+                        janusTraversal = traversal().withRemote(DriverRemoteConnection.using(cluster));
+                        // janusTraversal = (GraphTraversalSource) EmptyGraph.instance().configuration()..traversal().withRemote("conf/remote-graph.properties");
 
                         TinkerGraph tg = UserQuery2Gremlin.getSubGraphForQuery(janusTraversal, query, outputFiltersSingle);
                         GraphTraversalSource sg = tg.traversal();
+                        LOG.info("Graph result received, writing GraphML to " + graphMLFile);
                         sg.io(graphMLFile).write().iterate();
                         //  to convert to csv
                         OutputStream stream = new FileOutputStream(csvPath);
                         GremlinGraphWriter.graph_to_csv(tg, stream);
                         janusTraversal.close();
+                        janusTraversal = null;
+                        LOG.info("Janus query complete");
                     }
                     String csvChecksum = ListenerUtils.getChecksum(csvPath);
                     String graphMLChecksum = ListenerUtils.getChecksum(graphMLFile);
@@ -106,25 +126,33 @@ public class JobListener {
 
                     status.AddQueryResult(query.JobId(), query.UserId(), csvPath, csvChecksum);
                     status.AddQueryResult(query.JobId(), query.UserId(), graphMLFile, graphMLChecksum);
-
+                } catch (CompletionException e) {
+                    // Error with Janus. Log it, mark the job failed and keep going
+                    LOG.error("Error reading JanusGraph: " + e.getMessage());
+                    status.Update(query.JobId(), "FAILED");
+                } catch (SQLException e) {
+                    // Error with the metadatabase. Very bad since we can't report that
+                    // the user's job failed. Exit the loop
+                    throw e;
+                } catch(Exception e) {
+                    // Unknown error. We know it's not with the metadatabase as that
+                    // would have been caught above, so mark the job failed and exit
+                    // the loop
+                    status.Update(query.JobId(), "FAILED");
+                    throw e;
+                }
+                finally {
+                    if (janusTraversal != null) janusTraversal.close();
                     LOG.info("Deleting the sqs message");
                     DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
                             .queueUrl(queueUrl)
                             .receiptHandle(m.receiptHandle())
                             .build();
                     sqsClient.deleteMessage(deleteMessageRequest);
-                } catch (CompletionException e) {
-                    LOG.error("Error reading JanusGraph: " + e.getMessage());
-                    status.Update(query.JobId(), "FAILED");
-                } catch (SQLException e) {
-                    status.Update(query.JobId(), "FAILED");
-                    LOG.error("Error while updating meta db. Error is : " + e.getMessage());
-                    throw new Exception("Error while updating meta db", e);
-                }finally {
-                    if (janusTraversal != null) janusTraversal.close();
-                    status.Close();
                 }
-            }
+
+           }
+            status.Close();
         }
     }
 
