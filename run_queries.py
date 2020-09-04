e9d5b58bb09b5c3543328bc6008776189a24e174
Ben Fulton <befulton@iu.edu>
2020-09-04 08:59:56 -0400

    Support for Python 3.5

--- run_queries.py
+++ run_queries.py
@@ -4,19 +4,15 @@ import time
 
 target = "target/janus-job-listener-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 className = "iu.cadre.listeners.job.JanusConnection"
-config = "cadre_config.properties"
+config = "../lib/cadre_config.properties"
 
 for entry in os.scandir("queries"):
     if entry.path.endswith(".json") and entry.is_file():
-        print(str(entry))
-        test_process = subprocess.Popen(["java", "-cp", target, className, config, entry],
+        print(entry.name)
+        test_process = subprocess.Popen(["java", "-cp", target, className, config, entry.path],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         test_output, test_error = test_process.communicate()
         if test_process.returncode != 0:
-            print("Failure at %s" % entry)
+            print("Failure at %s" % entry.name)
             print(test_error)
         time.sleep(5)
-
-
-
-
