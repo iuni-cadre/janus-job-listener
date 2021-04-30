b106a8d9d07aeb3337db701b6dc5650ab4f579c4
Ubuntu <ubuntu@ip-10-0-1-105.us-east-2.compute.internal>
2021-04-30 01:38:10 +0000

    updating script to have custom names

--- run_queries_multiple.py
+++ run_queries_multiple.py
@@ -4,6 +4,7 @@ import time
 import pathlib
 import sys
 import json
+from shutil import copyfile
 
 target = "target/janus-job-listener-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 className = "iu.cadre.listeners.job.JanusConnection"
@@ -18,34 +19,50 @@ if len(sys.argv) > 1:
 
 for entry in os.scandir("test_queries_sph"):
     if entry.path.endswith(".json") and entry.is_file():
-        print(entry.name)_
-        with open('filename') as f:
+        print(entry.name)
+        with open(vertex_field_value_file) as f:
             lines = f.readlines()
         for line in lines:
+            print(line)
             with open(entry.path) as json_file:
                 data = json.load(json_file)
+                json_file.close()
                 graph = data["graph"]
                 nodes = graph["nodes"]
                 for node in nodes:
                     node_vertex_type = node["vertexType"]
-                    if vertex_type is node_vertex_type:
+                    print(node_vertex_type)
+                    if vertex_type == node_vertex_type:
+                        print("********")
                         filters = node["filters"]
                         for filter in filters:
-                            filed = filter["field"]
-                            if field is vertex_field_name:
-                                filed["value"] = line
+                            field = filter["field"]
+                            if field == vertex_field_name:
+                                filter["value"] = line
+                                print(line)
 
             with open(entry.path, "w") as jsonFile:
+                print(data)
                 json.dump(data, jsonFile)
+                json_file.close()
 
+            author_name = line.replace(" ", "")
+            author_name = author_name.rstrip()
+            print(author_name)
+            json_file_author = "test_queries_sph/" + author_name + ".json"
+            copyfile(entry.path, json_file_author)
+           
             t0 = time.time()
-            test_process = subprocess.Popen(["java", "-cp", target, className, config, entry.path],
+            test_process = subprocess.Popen(["java", "-cp", target, className, config, json_file_author],
                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             test_output, test_error = test_process.communicate()
             if test_process.returncode != 0:
                 print("Failure at %s" % entry.name)
                 print(test_error)
-            outfile = "test_queries/" + str(pathlib.PurePath(entry.name).with_suffix('')) + "_edges.csv"
+            author_name = line.replace(" ", "")
+            author_name = author_name.rstrip()
+            print(author_name)
+            outfile = "test_queries_sph/"  +author_name +  "_edges.csv"
             linecount = int(subprocess.check_output(["wc", "-l", outfile]).decode("utf8").split()[0])
             print("%s results returned in %s seconds" % (linecount, time.time() - t0))
 
